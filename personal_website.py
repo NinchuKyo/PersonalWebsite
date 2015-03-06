2d487b7da1fd8b053ccfc3620fb1a3c8b12e6b13
Duong Nguyen <davidnguyenT@gmail.com>
2015-03-06 17:17:55 -0600

    Add Procfile and port configuration to app

--- personal_website.py
+++ personal_website.py
@@ -5,9 +5,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello():
     return "Hello, World!"
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 33507))
+    app.run(debug=True, port=port)
