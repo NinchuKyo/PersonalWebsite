dc2e16e1f132b6871b6e01c9b602043365dcde7b
Duong Nguyen <davidnguyenT@gmail.com>
2015-03-06 18:19:51 -0600

    Add arguments

--- personal_website.py
+++ personal_website.py
@@ -1,13 +1,14 @@
 #!usr/bin/env python
 
-import os
+import os 
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return '<h1>Hello, World!<h1>'
 
 if __name__ == '__main__':
-    app.run()
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=False, host='0.0.0.0', port=port)
