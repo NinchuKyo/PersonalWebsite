12e824b97367d171554c1e74b642191a9c4198e1
Duong Nguyen <davidnguyenT@gmail.com>
2015-07-18 14:56:31 -0500

    Add TODO style navigation on Projects page Add functionality for disabled Javascript on browsers, and fix navbar location

--- static/js/layout.js
+++ static/js/layout.js
@@ -22,11 +22,40 @@ fallback.ready(function() {
     $(document).ready(function() {
         $('.scrollspy').scrollSpy();
     });
+    
     $(window).load(function() { 
         $('.parallax').parallax();
         $('.parallax').hide(0).delay(500).fadeIn(1000);
         $('#header-text').hide(0).delay(1200).fadeIn(2000);
         $('#sub-header').removeClass("white-text").addClass("light-green-text");
     });
-    $('.button-collapse').sideNav();
+    
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > $('#thiswebsite').offset().top)
+        {
+            $('#todo_site').html('<i class="fa fa-check-square-o"></i> About This Site');
+        }
+        
+        if ($(this).scrollTop() > $('#note').offset().top)
+        {
+            $('#todo_note').html('<i class="fa fa-check-square-o"></i> _Note');
+        }
+        
+        if ($(this).scrollTop() > $('#sharepointreporting').offset().top)
+        {
+            $('#todo_sharepoint').html('<i class="fa fa-check-square-o"></i> SharePoint Reporting Tool');
+        }
+
+        if ($(this).scrollTop() > $('#twitterdatamining').offset().top)
+        {
+            $('#todo_twitter').html('<i class="fa fa-check-square-o"></i> Twitter Data Mining');
+        }
+
+        if ($(this).scrollTop() > $('#tts').offset().top)
+        {
+            $('#todo_tts').html('<i class="fa fa-check-square-o"></i> Task Tracker System');
+        }
+    }); 
+    
+    $('.button-collapse').sideNav();     
 });
